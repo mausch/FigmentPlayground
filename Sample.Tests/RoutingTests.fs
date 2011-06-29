f1e2f73a8d2111e5fae817fd364472eccb9aeac2
mausch <mauricioscheffer@gmail.com>
2011-06-29 00:35:33 -0300

    PUT /hi test

--- Sample.Tests/RoutingTests.fs
+++ Sample.Tests/RoutingTests.fs
@@ -2,14 +2,16 @@
 
 open Xunit
 open System
+open System.Collections.Specialized
 open System.Text
 open System.Web
 open System.Web.Mvc
 open System.Web.Routing
+open Figment.Helpers
 open Figment.Testing
 
 [<Fact>]
-let ``get /`` () =
+let ``get / redirects to /hi`` () =
     RouteTable.Routes.Clear()
     SampleApp.Actions.webActions()
     let route,controller = getController "GET" ""
@@ -22,7 +24,7 @@ let ``get /`` () =
     Assert.Equal("hi", !redirectUrl)
 
 [<Fact>]
-let ``get /hi`` () =
+let ``get /hi shows hello world`` () =
     RouteTable.Routes.Clear()
     SampleApp.Actions.webActions()
     let route,controller = getController "GET" "hi"
@@ -33,3 +35,27 @@ let ``get /hi`` () =
                         sb.Append s |> ignore }
     controller.Execute ctx
     Assert.Contains("Hello World", sb.ToString())
+
+[<Fact>]
+let ``put /hi returns method not allowed``() =
+    RouteTable.Routes.Clear()
+    SampleApp.Actions.webActions()
+    SampleApp.Actions.genericActions()
+    let route,controller = getController "PUT" "hi"
+    let statusCode = ref 0
+    let nv = NameValueCollection()
+    let ctx = buildResponse route
+                { new HttpResponseBase() with
+                    override y.AppendHeader(name, value) =
+                        nv.Add(name, value)
+                    override y.StatusCode 
+                        with get() = !statusCode
+                        and set v = statusCode := v }
+    ctx.HttpContext <- 
+        { new DelegatingHttpContextBase(ctx.HttpContext) with
+            override x.Request = (buildRequest "PUT" "hi").Request }
+    controller.Execute ctx
+    Assert.Equal(405, !statusCode)
+    Assert.Equal("GET, HEAD", nv.["Allow"])
+    
+
